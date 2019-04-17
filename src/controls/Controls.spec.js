ae243e186f32d64f9d7d2f4837095cc71796571e
edreeseg <edward.reeseg@gmail.com>
2019-04-16 20:12:55 -0400

    Basic tests complete, MVP achieved

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,64 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, fireEvent, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Controls from './Controls';
+afterEach(cleanup);
+
+describe('<Controls />', () => {
+  it('Should render without crashing', () => {
+    render(<Controls />);
+  });
+  it('Should match snapshot', () => {
+    const tree = renderer.create(<Controls />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+  it('Should provide button to close gate', () => {
+    const { getByText } = render(<Controls />);
+    getByText(/Close Gate/i);
+  });
+  it('Should provide button to lock gate', () => {
+    const { getByText } = render(<Controls />);
+    getByText(/^Lock Gate/i);
+  });
+  it('Should disable lock button if gate is open', () => {
+    const { queryByText } = render(<Controls />);
+    const button = queryByText(/^Lock Gate/i);
+    expect(button).toBeDisabled();
+  });
+  it('Should disable open button if gate is locked', () => {
+    const { queryByText } = render(<Controls locked closed />);
+    const openButton = queryByText(/Open Gate/i);
+    expect(openButton).toBeDisabled();
+  });
+  it('Should fire toggleClosed function when close button is clicked', () => {
+    const mock = jest.fn();
+    const { queryByText } = render(<Controls toggleClosed={mock} />);
+    const closeButton = queryByText(/Close Gate/i);
+    fireEvent.click(closeButton);
+    expect(mock).toHaveBeenCalled();
+  });
+  it('Should not fire toggleClosed function when close button is clicked while gate is locked', () => {
+    const mock = jest.fn();
+    const { queryByText } = render(
+      <Controls toggleClosed={mock} closed locked />
+    );
+    const openButton = queryByText(/Open Gate/i);
+    fireEvent.click(openButton);
+    expect(mock).not.toHaveBeenCalled();
+  });
+  it('Should fire toggleLocked function when lock button is clicked', () => {
+    const mock = jest.fn();
+    const { queryByText } = render(<Controls toggleLocked={mock} closed />);
+    const lockButton = queryByText(/^Lock Gate/i);
+    fireEvent.click(lockButton);
+    expect(mock).toHaveBeenCalled();
+  });
+  it('Should not fire toggleLocked function when lock button is clicked while gate is open', () => {
+    const mock = jest.fn();
+    const { queryByText } = render(<Controls toggleLocked={mock} />);
+    const lockButton = queryByText(/^Lock Gate/i);
+    fireEvent.click(lockButton);
+    expect(mock).not.toHaveBeenCalled();
+  });
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,52 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, cleanup } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Display from './Display';
+afterEach(cleanup);
+
+describe('<Display />', () => {
+  it('Should render without crashing', () => {
+    render(<Display />);
+  });
+  it('Should match snapshot', () => {
+    const tree = renderer.create(<Display />);
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+  it('Should display gate as open by default', () => {
+    const { getByText } = render(<Display />);
+    getByText(/Open/i);
+  });
+  it('Should display gate as unlocked by default', () => {
+    const { getByText } = render(<Display />);
+    getByText(/Unlocked/i);
+  });
+  it('Should display gate as closed if closed prop is passed', () => {
+    const { getByText } = render(<Display closed />);
+    getByText(/Closed/i);
+  });
+  it('Should display gate as locked if locked prop is passed', () => {
+    const { getByText } = render(<Display locked />);
+    getByText(/^Locked/i);
+  });
+  it('Should use red-led class when locked', () => {
+    const { queryByText } = render(<Display locked />);
+    const status = queryByText(/^Locked/i);
+    expect(status).toHaveClass('red-led');
+  });
+  it('Should use red-led class when closed', () => {
+    const { queryByText } = render(<Display closed />);
+    const status = queryByText(/Closed/i);
+    expect(status).toHaveClass('red-led');
+  });
+  it('Should use green-led class when unlocked', () => {
+    const { queryByText } = render(<Display />);
+    const status = queryByText(/Unlocked/i);
+    expect(status).toHaveClass('green-led');
+  });
+  it('Should use green-led class when open', () => {
+    const { queryByText } = render(<Display />);
+    const status = queryByText(/Open/i);
+    expect(status).toHaveClass('green-led');
+  });
+});
